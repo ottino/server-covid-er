b6dcf28dfbb019e6431f25d7b756a4433b85ec41
ottino <maxi.ottino@gmail.com>
2020-09-07 23:35:24 -0300

    Modulo para trabajar la base con express. Se modifica app para ser publicada en heroku

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const driver = require('./controlador/driver') ;
+
+// Si no existe va 6969. conf para utilizar Heroku
+const puerto = process.env.PORT || 6969;
+
+app.get('/', (req, res)=>{
+
+    let items = driver.getItems();
+    //items.push( req.url );
+    res.send( items );
+
+});
+
+app.post('/agregar', function(req, res) {
+
+     console.log(JSON.stringify(req.body));
+
+     let parametros = req.query;
+
+     driver.crearItem( parametros['link'] , parametros['fecha'] );
+
+     res.send(  parametros  );
+});
+
+app.listen(puerto, 
+    ()=> console.log(`Escuchando el puerto ${ puerto }`) );
